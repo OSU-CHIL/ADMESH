98a3b4ce354fbf7ca74e048f83248bc3760613bf
Younghun Kang <younghun@utexas.edu>
2024-03-10 16:18:24 -0500

    Compile mex files at startup

--- libraries/CompileMEX.m
+++ libraries/CompileMEX.m
@@ -1,22 +1,44 @@
-temp = dir('libraries/admesh library/**/*.c');
+function CompileMEX
 
-PWD = pwd;
+% Find all .c files in admesh library
+Subfolders = {'admesh library','InsidePolyFolder'};
+c_files = [];
+for i = 1 : length(Subfolders)
+    c_files = [c_files; dir(['libraries/',Subfolders{i},'/**/*.c'])];
+    c_files = c_files(:);
+end
 
-for i = 1 : length(temp)
+% Identify platform and set extension of mex file
+if ismac
+    [~,result] = system('uname -m');
+    if strcmpi(result,'arm64')
+        extension = '.mexmaca64';
+    else
+        extension = '.mexmaci64';
+    end
+elseif isunix
+    extension = '.mexa64';
+elseif ispc
+    extension = '.mexw64';
+else
+    error('Platform cannot be identified.')
+end
 
-    folder = temp(i).folder;
-    name = temp(i).name;
+% Compile mex files over loop
+PWD = pwd; % Keep current folder
+for i = 1 : length(c_files)
+
+    folder = c_files(i).folder;
+    name = c_files(i).name;
 
     foldername = [folder,'/',name];
-
-    foldername2 = strrep(foldername,'.c','.mex*');
+    foldername_mex = strrep(foldername,'.c',extension);
     
-    exist(foldername2,'file')
-
-    if ~exist(foldername2,'file')
+    if ~exist(foldername_mex,'file')
         cd(folder);
         mex(foldername);
     end
 end
 
+% Return to original folder
 cd(PWD);
