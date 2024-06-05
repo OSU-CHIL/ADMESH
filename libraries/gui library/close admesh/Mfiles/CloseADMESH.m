a5b7a1b138b19ce92ebe848fc8ab2b2bdc6ee841
Younghun Kang <younghun@utexas.edu>
2024-06-05 17:39:59 -0500

    Close subapps when closing ADMESH
    Former-commit-id: bdc1b732f94daf4302e53fcb4e6e41f28d4a7e4f

--- libraries/gui library/close admesh/Mfiles/CloseADMESH.m
+++ libraries/gui library/close admesh/Mfiles/CloseADMESH.m
@@ -1,4 +1,4 @@
-function CloseADMESH(varargin)
+function CloseADMESH(app)
 
 msg = 'Are you sure you want to close ADMESH?';
 choice = uiconfirm(app.UIFigure,msg,'ADMESH',...
@@ -8,12 +8,21 @@ drawnow; pause(.005);
 switch choice
     
     case 'Yes'
+        % Close subapps
+        for i = 1 : length(app.subapps)
+            subappnames = fieldnames(app.subapps);
+            subapp = app.subapps.(subappnames{i});
+            if isvalid(subapp)
+                delete(subapp);
+            end
+        end
+        % Close ADMESH app
+        delete(app);
+        % Remove libraries from path
         warning_status = warning;
         warning('off');
         rmpath(genpath('libraries'));
         warning(warning_status);
-        delete(varargin{1})
-
     case 'No'
         
         
