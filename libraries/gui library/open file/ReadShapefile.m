6d0e99608ac183204c39662ee343ec0c1faec9a4
Younghun Kang <younghun@utexas.edu>
2024-06-11 16:37:38 -0500

    Revert "Add options to add .shp file to current app.PTS"
    This reverts commit 6174e7f692d571bf6b17b47b66165c7daf5f7153 [formerly 3216e0f7e2fc218e8d2ebfe80d94ccc9838dfe60].
    
    
    Former-commit-id: 05140666dd1e25f1367e6a15b6b23425bf4c100c

--- libraries/gui library/open file/ReadShapefile.m
+++ libraries/gui library/open file/ReadShapefile.m
@@ -1,47 +1,22 @@
 function [app,status] = ReadShapefile(file,app)
 
-% Read SHP file
+status = 1;
 try
     SHP = shaperead(file);
-    status = 1;
 catch
     status = 0;
+end
+    
+if status == 0
     return;
 end
-
-% Add SHP file to PTS variable
-PTS = app.PTS;
-if isfield(PTS,'Poly')
-    n = length(PTS.Poly);
-else
-    n = 0;
-end
-
-% Add empty attributes if original PTS was empty
-if isempty(PTS)
-    PTS.Poly = [];
-    PTS.Constraints = [];
-    PTS.cpplon = [];
-    PTS.cpplat = [];
-end
-
-flag = 1;
 for i = 1 : length(SHP)
-    % check if attribute already exists in PTS
-    for j = 1 : length(PTS.Poly)
-        if isequal(PTS.Poly(j).x, SHP(i).X(:)) && isequal(PTS.Poly(j).y, SHP(i).Y(:))
-            flag = 0;
-            break;
-        end
-    end
-
-    % Add to PTS if it's new
-    if flag
-        PTS.Poly(n+i).x = SHP(i).X(:);
-        PTS.Poly(n+i).y = SHP(i).Y(:);
-    end
+    PTS.Poly(i).x = SHP(i).X(:);
+    PTS.Poly(i).y = SHP(i).Y(:);    
 end
 
-% Save to app
-app.PTS = PTS;
+PTS.Constraints = [];
+PTS.cpplon = [];
+PTS.cpplat = [];
 
+app.PTS = PTS;
